3feea2afedde4b5682a1411858eaf22b9c66350e
Julia Savitskaya <Savitskaya@ap-team.ru>
2022-11-08 10:19:29 +0300

    first part

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('dist'));
+// sendFile will go here
+/*app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});*/
+app.listen(PORT, function () {
+    console.log('Server started at http://localhost:' + PORT);
+});
